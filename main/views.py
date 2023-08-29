1179531d3e16d42f57e678ef0c6b3de5c1f48547
Kauet758 <kauetoliveira25@gmail.com>
2023-08-29 15:09:27 -0300

    feito
    O alunoID já foi feito

--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from .models import Aluno
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-
+####################
 def alunoView(request):
     alunos_list = Aluno.objects.all()
     return render(request, 'main/alunos.html', {'alunos_list':alunos_list})
+####################
+
+def alunoIDview(request, id):
+    aluno = get_object_or_404(Aluno, pk=id)
+    print(aluno)
+    return render(request, 'main/alunoID.html', {'aluno':aluno})
 
 # Create your views here.
