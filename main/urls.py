b6550fd6603a1119cda4d36573ecf3cafd78554e
Kauet758 <kauetoliveira25@gmail.com>
2023-08-30 01:33:23 -0300

    resto
    entender o que esta acontecendo no resto do slide.

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.alunoView, name='aluno-lista'),
-    
+    #*15
+    path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'),
+
+
 ]
 
