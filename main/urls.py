3323a7d8975ace6295d8a7e024ba14f9b7022592
Kauet758 <140269456+Kauet758@users.noreply.github.com>
2023-09-02 16:45:15 -0500

    02/03/2023
    terminar em casa.

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from . import views
-
+from .import views
+from.views import AlunoCreateView, AlunoUpdateView
 urlpatterns = [
     path('', views.alunoView, name='aluno-lista'),
     #*15
     path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'),
-
+    #*20
+    path('aluno/create/', AlunoCreateView.as_view(), name='aluno-create'),
+    path('alono/<int:pk>/update/', AlunoUpdateView.as_view(), name='aluno-upadate'),
 
 ]
 
