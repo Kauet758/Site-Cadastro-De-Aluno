553efe8ec20c9f667f40088282b1350309adf5cd
Kauet758 <kauetoliveira25@gmail.com>
2023-09-08 22:34:54 -0300

    08/09/23
    pegar com arthur o resto do código

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,9 @@
+
 from django.urls import path
 from .import views
-from.views import AlunoCreateView, AlunoUpdateView
+
 urlpatterns = [
     path('', views.alunoView, name='aluno-lista'),
-    #*15
+    ###################################
     path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'),
-    #*15
-    path('aluno/create/', AlunoCreateView.as_view(), name='aluno-create'),
-    path('aluno/int:pk>/update/', AlunoUpdateView.as_view(), name='aluno-update'),
-
-
 ]
-
